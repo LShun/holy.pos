8f6210e7c188b970ad976305127c9296b5047101
CChun24 <nyanchun24@gmail.com>
2019-06-24 01:15:57 +0800

    (Order) Added empty class Cart
    Added empty class Cart

--- src/main/java/order/Order.java
+++ src/main/java/order/Order.java
@@ -4,3 +4,18 @@ public class Order {
     public static void order() {
     }
 }
+
+//This is a visual cart to record the items purchased by customer/scan by staff
+class Cart{
+    private ArrayList<Product> items; //List of items in cart
+    private int count;                //Total number of item in cart
+
+    Cart(){
+        this.items = new ArrayList<Product>();
+        this.count = 0;
+    }
+    public void add(int index){}//Add an item into cart}
+    public void del(int index){}//Del an item from cart}
+    public void proceed(){}//Calculate price}
+    public void clear(){}//Clear the cart}
+}
