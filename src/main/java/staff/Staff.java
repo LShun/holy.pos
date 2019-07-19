d90617ff7a2c793a793625c567f1189f00680bd3
CChun24 <nyanchun24@gmail.com>
2019-07-19 23:37:06 +0800

    Added staff.add

--- src/main/java/staff/Staff.java
+++ src/main/java/staff/Staff.java
@@ -1,13 +1,14 @@
 package staff;
 
-import java.awt.*;
 import java.util.Scanner;
 //import java.util.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Staff {
 
     private String staffID;
+    private String password;
     private String sName;
     private char gender;
     private static int day;
@@ -21,25 +22,21 @@ public class Staff {
 
    // }
 
-    public Staff(String staffID, String sName, char gender,String designation, int workingExperience,
-                int year,int month, int dayOfMonth){
+    public static ArrayList<Staff> employeeList = new ArrayList<Staff>();
 
-        this.staffID=staffID;
-        this.sName=sName;
-        this.gender=gender;
-        this.designation=designation;
-        this.workingExperience=workingExperience;
-        this.dateOfEmployed.of(year, month, dayOfMonth);
+    public Staff(String staffID, String password, String sName, char gender,String designation, int workingExperience,
+                int year,int month, int dayOfMonth){
+        this.setStaffID(staffID);
+        this.setPassword(password);
+        this.setsName(sName);
+        this.setGender(gender);
+        this.setDesignation(designation);
+        this.setWorkingExperience(workingExperience);
+        this.dateOfEmployed = LocalDate.of(year, month, dayOfMonth);
     }
     
-    public Staff(String staffID, String sName, char gender,String designation, int workingExperience){
-
-        this.staffID=staffID;
-        this.sName=sName;
-        this.gender=gender;
-        this.designation=designation;
-        this.workingExperience=workingExperience;
-        this.dateOfEmployed.of(0,0,0);
+    public Staff(String staffID, String password, String sName, char gender,String designation, int workingExperience){
+        this(staffID, password, sName, gender, designation, workingExperience, 0, 0, 0);
     }
 
     public static void clearScreen() {
@@ -51,7 +48,7 @@ public class Staff {
     {
         //Local Variables declaration
         int selection;
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         //Display homescreen
        // homescreen();
 
@@ -71,6 +68,7 @@ public class Staff {
 
         return scan.nextInt();
     }
+
     public static void staff(){
         //Variables declaration
         int choice;
@@ -86,10 +84,47 @@ public class Staff {
             {
                 case 1:
                     System.out.println("Add Staff Record");
+                    {
+                        String staffID, password, sName, designation;
+                        char gender;
+                        int workingExperience;
+                        String[] dateOfEmployed;
+                        Scanner scan = new Scanner(System.in);
+
+                        System.out.println("Enter staff Id : ");
+                        staffID = scan.nextLine();
+                        System.out.println("Enter staff password : ");
+                        password = scan.nextLine();
+                        System.out.println("Enter staff name : ");
+                        sName = scan.nextLine();
+                        System.out.println("Enter staff gender : ");
+                        gender = scan.nextLine().charAt(0);
+                        System.out.println("Enter staff designation : ");
+                        designation = scan.nextLine();
+                        System.out.println("Enter staff working experience : ");
+                        workingExperience = scan.nextInt();
+                        scan.nextLine();
+                        System.out.println("Enter staff date of employed (yy-mm-dd): ");
+                        dateOfEmployed = scan.nextLine().split("[-/]");
+
+                        employeeList.add(new Staff(staffID, password, sName, gender,
+                                designation, workingExperience,
+                                Integer.parseInt(dateOfEmployed[0]),
+                                Integer.parseInt(dateOfEmployed[1]),
+                                Integer.parseInt(dateOfEmployed[2]))
+                        );
+                    }
                     break;
 
                 case 2:
                     System.out.println("Display Staff Record");
+                    for(int i = 0; i< employeeList.size(); i++){
+                        Staff temp = employeeList.get(i);
+                        System.out.printf("%-8s%-16s%-16s%2c%4d%15s",
+                                temp.getStaffID(), temp.getPassword(),temp.getsName(),
+                                temp.getGender(), temp.getWorkingExperience(),
+                                temp.getDateOfEmployed().toString());
+                    }
                     break;
 
                 case 3:
@@ -105,22 +140,28 @@ public class Staff {
 
                 default:
                     System.out.println("Please enter a valid number");
-
-
             }
 
             System.out.println("\n");
 
             choice = mainMenu();
         } while (choice != 7);
-
-
-
-
     }
 
-
-
+    public String getStaffID() { return staffID; }
+    public void setStaffID(String staffID) { this.staffID = staffID; }
+    public String getsName() { return sName; }
+    public void setsName(String sName) { this.sName = sName; }
+    public char getGender() { return gender; }
+    public void setGender(char gender) { this.gender = gender; }
+    public String getDesignation() { return designation; }
+    public void setDesignation(String designation) { this.designation = designation; }
+    public int getWorkingExperience() { return workingExperience; }
+    public void setWorkingExperience(int workingExperience) { this.workingExperience = workingExperience; }
+    public LocalDate getDateOfEmployed() { return dateOfEmployed; }
+    public void setDateOfEmployed(LocalDate dateOfEmployed) { this.dateOfEmployed = dateOfEmployed; }
+    public String getPassword(){ return password; }
+    public void setPassword(String password){ this.password = password; }
 }
 
 
