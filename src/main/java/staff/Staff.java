387bf35ae2dd83620efe7a6649bbceacefc7481b
CChun24 <nyanchun24@gmail.com>
2019-07-27 01:40:42 +0800

    Added basic function of modify and search
    Changed getsName, setsName to getName,setName
    I dunno if junrong want :new_moon_with_face:  :yum:

--- src/main/java/staff/Staff.java
+++ src/main/java/staff/Staff.java
@@ -1,8 +1,11 @@
 package staff;
 
+import pub.vScan;
+
 import java.util.Scanner;
 //import java.util.Date;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Staff {
@@ -21,20 +24,25 @@ public class Staff {
 
    // }
 
-    public static ArrayList<Staff> employeeList = new ArrayList<Staff>();
+    public static ArrayList<Staff> employeeList = new ArrayList<Staff>(Arrays.asList(
+            new Staff("0001","123","Long Shun",'M',"Holy 1",2001,1,1),
+            new Staff("0002","456","Jun Rong",'M',"Holy 2",2000,2,2),
+            new Staff("0003","789","Shannen",'F',"Holy 3",2000,3,3),
+            new Staff("0004","000","Kim Chun",'M',"Holy 4",2000,4,4)
+    ));
 
     public Staff(String staffID, String password, String sName, char gender,String designation,
                 int year,int month, int dayOfMonth){
         this.setStaffID(staffID);
         this.setPassword(password);
-        this.setsName(sName);
+        this.setName(sName);
         this.setGender(gender);
         this.setDesignation(designation);
         this.dateOfEmployed = LocalDate.of(year, month, dayOfMonth);
     }
     
     public Staff(String staffID, String password, String sName, char gender,String designation){
-        this(staffID, password, sName, gender, designation,  0, 0, 0);
+        this(staffID, password, sName, gender, designation,  2019, 1, 1);
     }
 
 
@@ -88,21 +96,19 @@ public class Staff {
 
                 case 2:
                     System.out.println("Display Staff Record");
-                    for(int i = 0; i< employeeList.size(); i++){
-                        Staff temp = employeeList.get(i);
-                        System.out.printf("%-8s%-16s%-16s%2c%4d%15s",
-                                temp.getStaffID(), temp.getPassword(),temp.getsName(),
-                                temp.getGender(),
-                                temp.getDateOfEmployed().toString());
-                    }
+                    displayStaff(employeeList);
                     break;
 
                 case 3:
-                    System.out.println("");
+                    System.out.println("Modify Staff");
+                    ArrayList<Staff> result_Modify = searchStaff();
+                    modifyStaff(result_Modify);
                     break;
 
                 case 4:
-                    System.out.println("");
+                    System.out.println("Search Staff");
+                    ArrayList<Staff> result_Search = searchStaff();
+                    displayStaff(result_Search);
                     break;
 
                 case 5:
@@ -120,8 +126,8 @@ public class Staff {
 
     public String getStaffID() { return staffID; }
     public void setStaffID(String staffID) { this.staffID = staffID; }
-    public String getsName() { return sName; }
-    public void setsName(String sName) { this.sName = sName; }
+    public String getName() { return sName; }
+    public void setName(String sName) { this.sName = sName; }
     public char getGender() { return gender; }
     public void setGender(char gender) { this.gender = gender; }
     public String getDesignation() { return designation; }
@@ -167,6 +173,117 @@ public class Staff {
 
     }
 
+    private static void displayStaff(ArrayList<Staff> data) {
+        for(int i = 0; i< data.size(); i++){
+            Staff temp = data.get(i);
+            System.out.printf("%-8s%-16s%-16s%2c%15s\n",
+                    temp.getStaffID(), temp.getPassword(),temp.getName(),
+                    temp.getGender(),
+                    temp.getDateOfEmployed().toString());
+        }
+    }
+
+    private static void modifyStaff(ArrayList<Staff> data){
+        Staff staffToBeModified;
+
+        if(data.size() == 0) return; //Staff does not exists
+        else if(data.size() > 1){    //If more than one staff, user have to select which one to modify
+            displayStaff(data);
+            System.out.print("Enter the index : ");
+            int target = vScan.getInt();
+            staffToBeModified = data.get(target - 1);
+        }
+        else
+            staffToBeModified = data.get(0);
+
+        System.out.println("1. Staff ID");
+        System.out.println("2. Staff Name");
+        System.out.println("3. Staff Gender");
+        System.out.println("4. Staff Designation");
+        System.out.print("Select which one the modify : ");
+        int choice = vScan.getInt();
+        String inputString;
+        char inputChar;
+
+        switch(choice){
+            case 1:
+                System.out.print("Enter the staff Id : ");
+                inputString = vScan.getString();
+                staffToBeModified.setStaffID(inputString);
+                break;
+
+            case 2:
+                System.out.print("Enter the staff name : ");
+                inputString = vScan.getString();
+                staffToBeModified.setName(inputString);
+                break;
+            case 3:
+                System.out.print("Enter the staff gender : ");
+                inputChar = vScan.getChar();
+                staffToBeModified.setGender(inputChar);
+            case 4:
+                System.out.print("Enter the staff designation : ");
+                inputString = vScan.getString();
+                staffToBeModified.setDesignation(inputString);
+                break;
+            default:
+                System.out.println("Invalid value!");
+        }
+    }
+
+    private static ArrayList<Staff> searchStaff(){
+        String searchInput;
+        ArrayList<Staff> result = new ArrayList<>();
+
+        System.out.println("1. Staff ID");
+        System.out.println("2. Staff Name");
+        System.out.println("3. Staff Gender");
+        System.out.println("4. Staff Designation");
+        System.out.println("Select the search criteria : ");
+        int choice = vScan.getInt();
+
+        switch(choice) {
+            case 1:
+                System.out.print("Enter the staff Id : ");
+                searchInput = vScan.getString();
+
+                for (int i = 0; i < employeeList.size(); i++) {
+                    if (searchInput.equals(employeeList.get(i).getStaffID()))
+                        result.add(employeeList.get(i));
+                }
+                break;
+            case 2:
+                System.out.print("Enter the staff name : ");
+                searchInput = vScan.getString();
+
+                for (int i = 0; i < employeeList.size(); i++) {
+                    if (searchInput.equals(employeeList.get(i).getName()))
+                        result.add(employeeList.get(i));
+                }
+                break;
+            case 3:
+                System.out.print("Enter the staff gender : ");
+                searchInput = vScan.getString();
+
+                for (int i = 0; i < employeeList.size(); i++) {
+                    if (searchInput.equals(employeeList.get(i).getGender()))
+                        result.add(employeeList.get(i));
+                }
+                break;
+            case 4:
+                System.out.print("Enter the staff designation : ");
+                searchInput = vScan.getString();
+
+                for (int i = 0; i < employeeList.size(); i++) {
+                    if (searchInput.equals(employeeList.get(i).getDesignation()))
+                        result.add(employeeList.get(i));
+                }
+                break;
+            default:
+                System.out.println("Invalid value!");
+        }
+        return result;
+    }
 }
 
 
