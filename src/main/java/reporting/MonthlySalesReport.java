2edafa64164a5f27963f4ea9922231b6b7e1f3a6
Shannen <ongs-wm18@student.tarc.edu.my>
2019-08-11 17:21:50 +0800

    report
    create border

--- src/main/java/reporting/MonthlySalesReport.java
+++ src/main/java/reporting/MonthlySalesReport.java
@@ -4,6 +4,8 @@ import order.Order;
 import order.Receipt;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -26,7 +28,7 @@ public class MonthlySalesReport {
         Scanner scanner=new Scanner(System.in);
         int month;
 
-        System.out.println("\n\n1.January\n"+
+        System.out.print("\n\n1.January\n"+
                 "2.February\n"+
                 "3.March\n"+
                 "4.April\n"+
@@ -44,19 +46,24 @@ public class MonthlySalesReport {
 
 
         ArrayList<Object[]> rowData = new ArrayList<Object[]>();
+        double sum=0, tax=0;
         for(int i = 0; i < receipt.size(); i++){
             if(month!=receipt.get(i).getTransactionTime().getMonthValue())
                 continue;
 
-
             int sizeOfListOfItems = receipt.get(i).getListOfItems().size();
 
 
-            Object[] temp = new Object[7];
-            temp[0] = receipt.get(i).getTransactionTime();
-            //temp[1] = receipt.get(i).getBillID();
+            Object[] temp = new Object[4];
+            temp[0] = receipt.get(i).getTransactionTime().getYear()+" "+receipt.get(i).getTransactionTime().getMonthValue();
+            temp[1] = receipt.get(i).getBillID();
+            temp[2] = receipt.get(i).getTax();
+            temp[3] = receipt.get(i).getTotal();
 
-            for(int j = 0;j < sizeOfListOfItems ; j++){
+            sum+=receipt.get(i).getTotal();
+            tax+=receipt.get(i).getTax();
+
+            /*for(int j = 0;j < sizeOfListOfItems ; j++){
 
                 temp[1]=receipt.get(i).getListOfItems().get(j).getProduct().getId();
                 temp[2] = receipt.get(i).getListOfItems().get(j).getProduct().getTitle();
@@ -69,16 +76,18 @@ public class MonthlySalesReport {
                 temp = new Object[7];
                 temp[0] = "";
                 //temp[1] = "";
-            }
-
-            temp[0] = "Total Amount";
-            temp[2] = temp[3] = temp[4] = temp[5] = "";
-            temp[6] = receipt.get(i).getTotal();
+            }*/
             rowData.add(temp);
 
-            temp = new Object[7];
-            temp[0] = temp[6] = "";
-            rowData.add(temp);
+        }
+
+        for(int i=0;i<1;i++){
+            Object[] total=new Object[4];
+
+            total[0]=total[1]="";
+            total[2]="Total Tax Amount : " +tax;
+            total[3]="Total Bill Amount : " +sum;
+            rowData.add(total);
         }
 
         Object[][] realRowData = new Object[rowData.size()][];
@@ -87,7 +96,7 @@ public class MonthlySalesReport {
         }
 
         // Column Names
-        String[] columnNames = { "Date(Month)", "Product ID", "Product Name","Price(RM)","Quantity","Tax Amount(RM)","Amount(RM)"};
+        String[] columnNames = { "Date(Month)", "Bill ID","Tax Amount(RM)","Bill Amount(RM)"};
 
         // Initializing the JTable
         j = new JTable(realRowData, columnNames);
@@ -95,6 +104,25 @@ public class MonthlySalesReport {
 
         // adding it to JScrollPane
         JScrollPane sp = new JScrollPane(j);
+
+        JLabel label1 = new JLabel(new ImageIcon("C:\\Users\\User\\Desktop\\OOPT.jpeg"));
+        JLabel label2 = new JLabel("No. 1 & 2 Jalan 54, Desa Jaya, 52100, Kepong, Selangor, Malaysia");
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Monthly Sales Report", TitledBorder.CENTER,
+                TitledBorder.BOTTOM));
+
+        JTextField textfield = new JTextField("Date Generated: ");
+        textfield.setToolTipText("tooltip");
+
+        label1.setBounds(0,0, 454,388);
+        label2.setBounds(100,100, 1054,688);
+        panel.add(label1);
+        panel.add(label2);
+
+        f.add(panel, BorderLayout.NORTH);
+
         f.add(sp);
         // Frame Size
         f.setSize(500, 200);
