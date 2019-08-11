68e13a7eb6adb7845f875586d027525bbe36b7b3
CChun24 <nyanchun24@gmail.com>
2019-08-11 20:24:07 +0800

    Extract proceed()

--- src/main/java/order/Order.java
+++ src/main/java/order/Order.java
@@ -1,12 +1,13 @@
 package order;
 
-import auth.Auth;
 import auth.AuthV2;
 import pub.VScan;
 import food_menu.*;
 import staff.Staff;
 import staff.Worker;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -52,54 +53,7 @@ public class Order {
             )
     );
 
-//    private static boolean haveBeenInitialized = false;
-
     public static void order() {
-        /*
-        * 1. get worker id
-        * 2. display menu
-        * 3. select product
-        * 4. enter quantity
-        * 5. check if exists
-        * 6. Add to cart
-        * 7. Repeat
-        * 8. Proceed
-        * 9. Produce receipt
-        * */
-
-//        if(!haveBeenInitialized){
-//            haveBeenInitialized = true;
-
-//            Cart initialization_cart = new Cart("1906270001", Staff.getEmployeeList().get(0));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("LUBS"),2));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("BC"),2));
-//            receiptList.add(new Receipt(initialization_cart,50.0, LocalDateTime.of(2019,6,27,8,30,30)));
-
-//            initialization_cart = new Cart("1906270002", Staff.getEmployeeList().get(0));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("BDC"),2));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("BCH"),3));
-//            receiptList.add(new Receipt(initialization_cart,20.0,LocalDateTime.of(2019,6,27,10,00,25)));
-
-//            initialization_cart = new Cart("1906270003", Staff.getEmployeeList().get(1));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("LUBS"),3));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("BRSD"),1));
-//            receiptList.add(new Receipt(initialization_cart,36.0, LocalDateTime.of(2019,6,27,10,20,35)));
-
-//            initialization_cart = new Cart("1906270004", Staff.getEmployeeList().get(2));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("LUBS"),4));
-//            receiptList.add(new Receipt(initialization_cart,40.0, LocalDateTime.of(2019,6,27,10,50,57)));
-
-//            initialization_cart = new Cart("1906270005", Staff.getEmployeeList().get(3));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("LUBS"),4));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("BRSD"),10));
-//            receiptList.add(new Receipt(initialization_cart,150, LocalDateTime.of(2019,7,28,11,50,57)));
-
-//            initialization_cart = new Cart("1906270006", Staff.getEmployeeList().get(2));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("BDC"),2));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("LUBS"),4));
-//            initialization_cart.addOrMinus(new Item(FoodMenu.getProductByID("BCH"),1));
-//            receiptList.add(new Receipt(initialization_cart,40.0, LocalDateTime.of(2019,7,29,14,50,57)));
-//        }
 
         Worker session = AuthV2.getSession();
         if(session == null){
@@ -107,45 +61,51 @@ public class Order {
             return;
         }
 
-//        if(c != null && c.getWorker().equals(session)){
-//            System.out.println("There is still transaction undone last time.");
-//            System.out.println("Do you want to continue the transaction ?");
-//            System.out.println("Y - Yes | N- No");
-//            if(VScan.getChar() == 'N')
-//                c = new Cart();
-//        }else {
-//            c = new Cart();
-//        }
-
-        c = new Cart();
+        if(c != null && c.getWorker().equals(session)){
+            System.out.println("There is still transaction undone last time.");
+            System.out.println("Do you want to continue the transaction ?");
+            System.out.println("Y - Yes | N- No");
+            if(VScan.getChar() == 'N')
+                c = new Cart();
+        }else {
+            c = new Cart();
+        }
 
         int action;
-        double total;
 
-        orderMenu();
+        orderMenu(); //Display the menu
 
         //Action
         System.out.print("Enter the action code : ");
         action = VScan.getInt();
 
-        while(action != 4 || c.getListOfItems().isEmpty()) {
-            if (action == 1) {
-                addOrMinusProduct();
-            } else if (action == 2) {
-                delete();
-            } else if (action == 3) {
-                c.clearCart();
-            } else if (action == 4) {
-                if(c.getListOfItems().isEmpty())
-                    System.out.println("The cart is empty!");
-                else
+        while(action != 6) {
+            switch (action) {
+                case 1:
+                    addOrMinusProduct();
                     break;
-            } else if (action == 5) {
-                return;
-            } else {
-                System.out.println("Invalid action!");
+                case 2:
+                    c.display();
+                    break;
+                case 3:
+                    delete();
+                    break;
+                case 4:
+                    c.clearCart();
+                    break;
+                case 5:
+                    if (c.getListOfItems().isEmpty()) {
+                        System.out.println("The cart is empty!");
+                    }else {
+                        double total = c.getSubTotal() * (1 + Product.getTax());
+                        proceed(session, total);
+                    }
+                    break;
+                case 6:
+                    return;
+                default:
+                    System.out.println("Invalid action!");
             }
-            c.display();
 
             orderMenu();
 
@@ -153,10 +113,13 @@ public class Order {
             System.out.print("Enter the action code : ");
             action = VScan.getInt();
         }
+    }
 
-        total = c.getSubTotal();
+    private static void proceed(Worker session, double total) {
+        BigDecimal totalRounded = new BigDecimal(total);
+        totalRounded.setScale(0, RoundingMode.HALF_UP);
 
-        System.out.println("The total price is RM" + total);
+        System.out.println("The total price is RM" + totalRounded);
         System.out.print("Amount Received is RM");
         double amountReceived = VScan.getDouble();
         while(amountReceived < total) {
@@ -167,19 +130,22 @@ public class Order {
         double change = amountReceived - total;
         System.out.println("Change Amount is RM" + change);
 
-        Receipt r = new Receipt(c, amountReceived);
-        r.display();
+        Receipt r = new Receipt(c, amountReceived); //Create a Receipt obj
+        c = new Cart();      // Delete the reference to the cart obj
+        r.display();         // Display receipt
+        receiptList.add(r);  // Add the receipt into the ArrayList
 
-        receiptList.add(r);
-        Auth.s.setSalesReceived(Auth.s.getSalesReceived()+1);
+        //Increase the total sales made by this current session
+        session.setSalesReceived(session.getSalesReceived()+1);
     }
 
     private static void orderMenu() {
         System.out.println("1. Add Or Minus Products");
-        System.out.println("2. Delete Products");
-        System.out.println("3. Clear Cart");
-        System.out.println("4. Proceed");
-        System.out.println("5. Quit");
+        System.out.println("2. Display Added Products");
+        System.out.println("3. Delete Products");
+        System.out.println("4. Clear Cart");
+        System.out.println("5. Proceed");
+        System.out.println("6. Quit");
     }
 
     private static void delete() {
@@ -206,14 +172,13 @@ public class Order {
         //Show Menu
         FoodMenu.showProducts();
 
-        //Prompt input message
+        //Prompt input message for the product ID
         System.out.print("Enter the product code or -1 to quit : ");
         code = VScan.getString();
 
-        //When the input is not 'q'
+        //When the input is not '-1', get into the loop
         while (!code.equalsIgnoreCase("-1")) {
             product = FoodMenu.getProductByID(code);
-            //p = new Product(p.getId(), p.getTitle(), p.getDesc(), p.getPrice(), p.getTax());
 
             //When the product exists in the menu
             if (product.getId() != "") {
@@ -226,7 +191,6 @@ public class Order {
                 item = new Item(product, qty);
 
                 c.addOrMinus(item);
-                c.display();
             } else {
                 System.out.println("The code does not exists.");
             }
