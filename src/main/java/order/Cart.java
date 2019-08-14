0204aa2e07974177798d504817aeda65735e00d5
CChun24 <nyanchun24@gmail.com>
2019-08-14 22:28:20 +0800

    Fixed inport error

--- src/main/java/order/Cart.java
+++ src/main/java/order/Cart.java
@@ -10,7 +10,7 @@ import de.vandermeer.asciitable.AT_Row;
 import de.vandermeer.asciitable.AsciiTable;
 import de.vandermeer.asciitable.CWC_FixedWidth;
 import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
-import food_menu.Product;
+import menu.Product;
 import staff.Worker;
 
 public class Cart extends CartOrReceipt{
