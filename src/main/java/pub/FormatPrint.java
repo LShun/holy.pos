eeea362ac5718a7d984326d6350b92ef4c8297ab
equinox.bot <hiewlshun@gmail.com>
2019-08-15 21:23:04 +0800

    FormatPrint: Change to StringBuilder, remove extra import

--- src/main/java/pub/FormatPrint.java
+++ src/main/java/pub/FormatPrint.java
@@ -1,18 +1,15 @@
 package pub;
 
-import java.util.ArrayList;
-
 public class FormatPrint {
     // constants
-    final static int LINE_LENGTH = 85;
+    private final static int LINE_LENGTH = 85;
 
     public static void printHeader(String s) {
         int counts = (LINE_LENGTH - s.length()) / 2;
-        int remainder = (LINE_LENGTH - s.length()) % 2;
-        String padding = "";
+        StringBuilder padding = new StringBuilder();
 
         for (int i = 0; i < counts - 1; i++) {
-            padding += "=";
+            padding.append("=");
         }
 
         System.out.println("\n" + padding + " " + s.toUpperCase() + " " + padding + "\n");
